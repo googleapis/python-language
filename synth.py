fe14b80078c899685f5f86273bbb4cd3dd5dae56
Harikumar Devandla <hdevandla@google.com>
2020-10-07 19:49:59 -0700

    update synth.py

--- synth.py
+++ synth.py
@@ -42,9 +42,6 @@ templated_files = common.py_library(cov_level=99, samples=True, microgenerator=T
 
 s.move(templated_files, excludes=['.coveragerc'])
 
-# TODO(busunkim): Use latest sphinx after microgenerator transition
-s.replace("noxfile.py", """['"]sphinx['"]""", '"sphinx<3.0.0"')
-
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
 
 # ----------------------------------------------------------------------------
