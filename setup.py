828138b3c7c241c29c2628d3d8da15e47ca8463f
Bu Sun Kim <8822365+busunkim96@users.noreply.github.com>
2022-03-31 18:44:44 -0600

    fix(deps): require google-api-core >= 1.31.5, >= 2.3.2 on v1 release (#280)
    * chore(deps): allow google-api-core v2 on v1 release
    
    * ci: run unittest/lint/docs as gh actions
    
    Co-authored-by: Anthonios Partheniou <partheniou@google.com>

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ version = "1.3.0"
 # 'Development Status :: 5 - Production/Stable'
 release_status = "Development Status :: 5 - Production/Stable"
 dependencies = [
-    "google-api-core[grpc] >= 1.14.0, < 2.0.0dev",
+    "google-api-core[grpc] >= 1.31.5, <3.0.0dev,!=2.0.*,!=2.1.*,!=2.2.*,!=2.3.0",
     'enum34;python_version<"3.4"',
 ]
 extras = {}
